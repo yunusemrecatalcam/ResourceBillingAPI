c344959a8ab9ca78440f01224af8d89ddb99f51b
yunusemrecatalcam <yunus.catalcam@vircongroup.com>
2020-07-24 13:49:09 +0300

    ram usage calculation ok

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 This api queries prometheus and calculates resource usages for the desired container.
 
 ## Prometheus Queries
-CPU usage between now and given offset 
+####CPU usage between now and given offset 
 ```
 sum by (name) (container_cpu_usage_seconds_total{container_label_com_docker_swarm_service_name=~".*yves_sc_tlsnr.*"})-(sum by (name) (container_cpu_usage_seconds_total{container_label_com_docker_swarm_service_name=~".*yves_sc_tlsnr.*"} offset 5m))
 ```
-
- 
+####RAM usage summary between now and give offset in desired resolution
+```
+sum_over_time(container_memory_usage_bytes{container_label_com_docker_swarm_service_name="yves_sc_tlsnr"}[10m:1m]) 
+```
--- prometheus_ops.py
+++ prometheus_ops.py
@@ -9,7 +9,7 @@ class PrometheusOperator:
 
     @staticmethod
     def checkoffset(offset_string: str):
-        if offset_string[-1] == "m" or offset_string[-1] == "h" or offset_string[-1] == "d":
+        if offset_string[-1] in "smhd":
             return offset_string
         else:
             return "30d"
@@ -29,16 +29,42 @@ class PrometheusOperator:
         if query_response['status'] == "success":
             for container_metric_dict in query_response['data']['result']:
                 results_dict[container_metric_dict['metric']['name']] = container_metric_dict['value']
-                print(container_metric_dict['metric']['name'], container_metric_dict['value'])
+
+        return results_dict
+
+
+    def get_ram_total(self, label_name: str, label_value: str,
+                      offset_value: str, resolution:str):
+
+        offset_value = self.checkoffset(offset_value)
+        resolution = self.checkoffset(resolution)
+        query_string = 'sum_over_time(container_memory_usage_bytes{{{label}=~".*{label_value}.*"}}[{offset_value}:{resolution}])'.format(label=label_name,
+                                                                                                                    label_value=label_value,
+                                                                                                                    offset_value=offset_value,
+                                                                                                                    resolution=resolution)
+
+        response = requests.get(self.prometheus_address + '/api/v1/query',
+                                params={'query': query_string})
+        query_response = response.json()
+
+        results_dict = {}
+        if query_response['status'] == "success":
+            for container_metric_dict in query_response['data']['result']:
+                results_dict[container_metric_dict['metric']['name']] = container_metric_dict['value']
 
         return results_dict
 
 
 if __name__ == "__main__":
-    print("leley")
+
     prop = PrometheusOperator()
     for i in ['1m', '10m', '1h', '1d']:
         print(prop.get_cpu_total(label_name='container_label_com_docker_swarm_service_name',
                            label_value='yves_sc_tlsnr',
-                           offset_value=i))
+                           offset_value=i), i)
+        prop.get_ram_total(label_name='container_label_com_docker_swarm_service_name',
+                           label_value='yves_sc_tlsnr',
+                           offset_value="10m",
+                           resolution="1m")
         time.sleep(1)
+    print("lel")
