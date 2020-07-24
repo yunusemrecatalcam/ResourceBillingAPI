8a98cdedff0f82aab41c61930cf6b8257e3b5cf3
yunusemrecatalcam <yunus.catalcam@vircongroup.com>
2020-07-24 12:26:11 +0300

    total cpu usage func label_name and label_value parametrised

--- prometheus_ops.py
+++ prometheus_ops.py
@@ -7,23 +7,27 @@ class PrometheusOperator:
     def __init__(self):
         self.prometheus_address = os.environ['prom_addr']
 
-    def get_cpu_total(self):
+    def get_cpu_total(self, label_name: str, label_value: str):
+        query_string = 'sum by (name) (container_cpu_usage_seconds_total{{ {label}=~".*{label_value}.*"}})-' \
+                       '(sum by (name) (container_cpu_usage_seconds_total{{ {label}=~".*{label_value}.*"}} offset 5d))'.format(label=label_name,
+                                                                                                                               label_value=label_value)
         response = requests.get(self.prometheus_address + '/api/v1/query',
-                                params={
-                                    'query': 'sum by (name) (container_cpu_usage_seconds_total{container_label_com_docker_swarm_service_name=~".*yves_sc_tlsnr.*"})-(sum by (name) (container_cpu_usage_seconds_total{container_label_com_docker_swarm_service_name=~".*yves_sc_tlsnr.*"} offset 5d))'})
-
+                                params={'query': query_string})
         query_response = response.json()
-        if query_response['status'] == "success":
 
+        results_dict = {}
+        if query_response['status'] == "success":
             for container_metric_dict in query_response['data']['result']:
+                results_dict[container_metric_dict['metric']['name']] = container_metric_dict['value']
                 print(container_metric_dict['metric']['name'], container_metric_dict['value'])
 
-        return 0
+        return results_dict
 
 
 if __name__ == "__main__":
     print("leley")
     prop = PrometheusOperator()
     for i in range(10):
-        prop.get_cpu_total()
+        prop.get_cpu_total(label_name='container_label_com_docker_swarm_service_name',
+                           label_value='yves_sc_tlsnr')
         time.sleep(1)
